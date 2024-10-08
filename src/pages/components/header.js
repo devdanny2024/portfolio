1d2562d37cd9ef99fd0331af404b2c75d50e6d7e
Kayode <soliupeter@gmail.com>
2024-10-08 09:58:42 +0100

    Header first

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -3,9 +3,26 @@ import Image from 'next/image';
 function Header() {
     return(
     <>
-        
+        <div className='headers'>
+            <div className='header-cont'>
+                <div className='header-title'>
+                    <h1>Hello,</h1>
+                    <h2>I’m A Full-Stack Block Chain Developer</h2>
+                    <p>I am Olukayode Soliu, a full stack developer, great at a lot of other things too.</p>
+                    <div className='header-btn'>Send me a Mail</div>
+                </div>
+                <div className='header-img'>
+                    <Image
+                        src="/images/memage.png" // Path relative to the 'public' folder
+                        alt="Olukayode"
+                        width={400} // Specify width
+                        height={400} // Specify height
+                    />
+                </div>
+                
+            </div>
+        </div>
     </>
     )
 };
-
 export default Header;
