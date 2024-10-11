ad19688385eb6c90c244062be778f32c5a6fb4d4
Kayode <soliupeter@gmail.com>
2024-10-11 17:18:02 +0100

    as

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -8,27 +8,34 @@ function Footer() {
                 <h1>Social Bar</h1>
             </div>
             <div className='footer-socials'>
-                <Image
-                    className= 'fservice-icon'
-                    src="/github.svg" // Path relative to the 'public' folder
-                    alt="Olukayode"
-                    width={47} // Specify width
-                    height={47} // Specify height
-                />
-                <Image
-                    className= 'fservice-icon'
-                    src="/instagram.svg" // Path relative to the 'public' folder
-                    alt="Olukayode"
-                    width={47} // Specify width
-                    height={47} // Specify height
-                />
-                <Image
-                    className= 'fservice-icon'
-                    src="/twitter.svg" // Path relative to the 'public' folder
-                    alt="Olukayode"
-                    width={47} // Specify width
-                    height={47} // Specify height
-                />
+                <a href= 'https://github.com/devdanny2024'  target='_blank' rel='noopener noreferrer'>
+                    <Image
+                        className= 'fservice-icon'
+                        src="/github.svg" // Path relative to the 'public' folder
+                        alt="Olukayode"
+                        width={47} // Specify width
+                        height={47} // Specify height
+                    />
+                </a>
+                <a href='https://www.instagram.com/_itsofficialpete/profilecard/?igsh=bXRuZTd4eTVnODE3'  target='_blank' rel='noopener noreferrer'>
+                    <Image
+                        className= 'fservice-icon'
+                        src="/instagram.svg" // Path relative to the 'public' folder
+                        alt="Olukayode"
+                        width={47} // Specify width
+                        height={47} // Specify height
+                    />
+                </a>
+                <a href='https://x.com/theatarodo2nd?s=21'  target='_blank' rel='noopener noreferrer'>
+                    <Image
+                        className= 'fservice-icon'
+                        src="/twitter.svg" // Path relative to the 'public' folder
+                        alt="Olukayode"
+                        width={47} // Specify width
+                        height={47} // Specify height
+                    />
+                </a>
+                
             </div>
        </div>
     </>
