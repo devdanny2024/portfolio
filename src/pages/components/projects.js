cce4a12831db30ccd74ea17ee9dbfa58f0b579dc
Kayode <soliupeter@gmail.com>
2024-10-11 15:12:14 +0100

    lfg

--- src/pages/components/projects.js
+++ src/pages/components/projects.js
@@ -8,12 +8,48 @@ function Projects() {
                 <h1>Projects</h1>
                 <h2>Top Ones</h2>
             </div>
-            <div className='projects'>
-
+            <div className='proj-case'>
+                <div className ='project-row'>
+                    <div className = 'project1-img'></div>
+                    <div className='project-desc'>
+                        <h1>Software Development</h1>
+                        <h2>SUSI.AI</h2>
+                        <p>The main feature of the app is to provide a conversational 
+                            interface to provide intelligent answers using 
+                            the loklak/AskSusi infrastructure.
+                        </p>
+                        <h3><b>Platform:</b> Swift, SwiftUI.</h3>
+                        <div className='project-btn'>View Project</div>
+                    </div>
+                </div>
+                <div className ='project-row'>
+                    <div className='project-desc'>
+                        <h1>Website Development</h1>
+                        <h2>Hashiro</h2>
+                        <p>The main feature of the website is to offer a streamlined platform for trading NFTs, providing users with secure transactions, wallet integration, and intuitive collection browsing.
+                        </p>
+                        <h3><b>Platform:</b> Solidity. Next.js, Web3.js.</h3>
+                        <div className='project-btn'>View Project</div>
+                    </div>
+                    <div className = 'project2-img'></div>
+                </div>
+                <div className ='project-row'>
+                    <div className = 'project1-img'></div>
+                    <div className='project-desc'>
+                        <h1>Website Development</h1>
+                        <h2>SUSI.AI</h2>
+                        <p>The main feature of the app is to provide a conversational 
+                            interface to provide intelligent answers using 
+                            the loklak/AskSusi infrastructure.
+                        </p>
+                        <h3><b>Platform:</b> Swift, SwiftUI.</h3>
+                        <div className='project-btn'>View Project</div>
+                    </div>
+                </div>
             </div>
+            
         </div>
     </>
     )
 };
-
 export default Projects;
